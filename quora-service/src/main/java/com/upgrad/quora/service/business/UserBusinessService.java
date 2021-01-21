0a4f42e9b24dcc85c6726c96ac5fda18357a3997
snelpatel <snelpatel@gmail.com>
2021-01-21 17:15:59 +0530

    UserAuthEntity class created

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -1,28 +1,9 @@
 package com.upgrad.quora.service.business;
 
-import com.upgrad.quora.service.entity.UserEntity;
-import com.upgrad.quora.service.exception.SignOutRestrictedException;
-import com.upgrad.quora.service.exception.SignUpRestrictedException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 public class UserBusinessService {
 
-    @Autowired
-    private AdminBusinessService adminBusinessService;
-    @Transactional(propagation = Propagation.REQUIRED)
-
-    public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException {
-        return adminBusinessService.createUser(userEntity);
-    }
-
-    @Transactional(propagation = Propagation.REQUIRED)
-    public UserEntity signout(final String authorizationToken) throws SignOutRestrictedException {
-        return adminBusinessService.logoutUser(authorizationToken);
-    }
 
 }
