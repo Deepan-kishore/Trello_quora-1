7627d45b24b38294c5a4ed1efcbeb1a00a715a04
snelpatel <snelpatel@gmail.com>
2021-01-21 21:09:37 +0530

    QuestionBusinessService added

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -1,12 +1,14 @@
 package com.upgrad.quora.service.dao;
+
 import com.upgrad.quora.service.entity.QuestionEntity;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-
+@Repository
 public class QuestionDao {
     @PersistenceContext
     private EntityManager entityManager;
